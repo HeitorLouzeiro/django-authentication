50f32c8b800ccd8e6ba87a550f0a6033be4aad75
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-06-02 15:42:27 -0300

    Main file folder.

--- settings/settings.py
+++ settings/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+
 ]
 
 MIDDLEWARE = [
@@ -122,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# crispy forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
